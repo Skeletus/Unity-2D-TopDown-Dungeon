391df563c539825cc64998cca67e556e7d52a04c
Skeletus <Samol_88@hotmail.com>
2024-04-06 16:33:40 -0500

    creating base unity window for room node graph editor window

---
+++ Unity2DTopDownDungeonGame/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RoomNodeGraphEditor : EditorWindow
+{
+    [MenuItem("Room Node Graph Editor", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
+
+    private static void OpenWindow()
+    {
+        GetWindow<RoomNodeGraphEditor>("Room Node Graph Editor");
+    }
+}
