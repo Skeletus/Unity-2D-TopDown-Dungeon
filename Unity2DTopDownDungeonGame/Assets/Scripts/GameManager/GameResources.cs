f65da907e443ac5334b5267bbbe1dad07210c134
Skeletus <Samol_88@hotmail.com>
2024-04-08 21:22:30 -0500

    creating game resources script with singleton

---
+++ Unity2DTopDownDungeonGame/Assets/Scripts/GameManager/GameResources.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResources : MonoBehaviour
+{
+    private static GameResources instance;
+
+    public static GameResources Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = Resources.Load<GameResources>("GameResources");
+            }
+            return instance;
+        }
+    }
+
+    #region Header Region
+    [Space(10)]
+    [Header("DUNGEON")]
+    #endregion
+    #region Tooltip
+    [Tooltip("Populate with the dungeon RoomNodeTypeList Scriptable Object")]
+    #endregion
+
+    public RoomNodeTypeListSO roomNodeTypeList;
+}
