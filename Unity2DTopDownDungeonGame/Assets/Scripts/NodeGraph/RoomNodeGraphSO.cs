dbaa0cd2c5a078c22122dc34766e692065f3ac16
Skeletus <Samol_88@hotmail.com>
2024-04-13 22:59:46 -0500

    added load room node dictionary method in room node graph scriptable object

--- Unity2DTopDownDungeonGame/Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,12 +10,37 @@ public class RoomNodeGraphSO : ScriptableObject
     [HideInInspector] public List<RoomNodeSO> roomNodeList = new List<RoomNodeSO>();
     [HideInInspector] public Dictionary<string, RoomNodeSO> roomNodeDictionary = new Dictionary<string, RoomNodeSO>();
 
+    private void Awake()
+    {
+        LoadRoomNodeDictionary();
+    }
+
+    /// <summary>
+    /// Load the room node dictionary from the room node list
+    /// </summary>
+    private void LoadRoomNodeDictionary()
+    {
+        roomNodeDictionary.Clear();
+
+        // populate dictionary
+        foreach(RoomNodeSO node in roomNodeList)
+        {
+            roomNodeDictionary[node.id] = node;
+        }
+    }
+
     #region Editor Code
 
 #if UNITY_EDITOR
 
     [HideInInspector] public RoomNodeSO roomNodeToDrawLineFrom = null;
     [HideInInspector] public Vector2 linePosition;
+
+    // repopulate node dictionary every time a change is made in the editor
+    private void OnValidate()
+    {
+        LoadRoomNodeDictionary();
+    }
 
     public void SetNodeToDrawConnectionLines(RoomNodeSO roomNode, Vector2 position)
     {
