24f6b74e84ec50e07ee941c80a4a456511f677d0
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-04 22:41:38 -0500

    updated dungeon level so - check to make sure all the templates are specified for all the node types

--- Unity2DTopDownDungeonGame/Assets/Scripts/Dungeon/DungeonLevelSO.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Dungeon/DungeonLevelSO.cs
@@ -57,6 +57,52 @@ public class DungeonLevelSO : ScriptableObject
         {
             return;
         }
+
+        // check to make sure all the templates are specified for all the node types in the specified node graph
+
+        // first check that north/south corridor , east/west corridor and entrance types have been specified
+        bool isEWCorridor = false;
+        bool isNSCorridor = false;
+        bool isEntrance = false;
+
+        // looop throguh all room templates to check that this node type has been specified
+        foreach(RoomTemplateSO roomTemplateSO in roomTemplateList)
+        {
+            if (roomTemplateSO == null)
+            {
+                return;
+            }
+
+            if (roomTemplateSO.roomNodeType.isCorridorEW)
+            {
+                isEWCorridor = true;
+            }
+
+            if (roomTemplateSO.roomNodeType.isCorridorNS)
+            {
+                isNSCorridor = true;
+            }
+
+            if (roomTemplateSO.roomNodeType.isEntrance)
+            {
+                isEntrance = true;
+            }
+        }
+
+        if (isEWCorridor == false )
+        {
+            Debug.Log("In " + this.name.ToString() + " : No E/W Corridor Room Type Specified");
+        }
+        if (isNSCorridor == false)
+        {
+            Debug.Log("In " + this.name.ToString() + " : No S/N Corridor Room Type Specified");
+        }
+        if (isEntrance == false)
+        {
+            Debug.Log("In " + this.name.ToString() + " : No Entrance Room Type Specified");
+        }
+
+        // loop throguh all node graphs
     }
 #endif
     #endregion Validation
