06ef16bb590199f3b17d2b35bffcf85c98dfeab8
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-04 22:18:45 -0500

    updated dungeonLevelSO added two more attributes

--- Unity2DTopDownDungeonGame/Assets/Scripts/Dungeon/DungeonLevelSO.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Dungeon/DungeonLevelSO.cs
@@ -21,5 +21,22 @@ public class DungeonLevelSO : ScriptableObject
     [Header("Room templates for level")]
     #endregion Header ROOM TEMPLATES FOR LEVEL
 
+    #region Tooltip
+    [Tooltip("Populate the list with the room templates that you want to be part of the level" +
+        "You need to ensure that room templates are included for all room nodes types " +
+        "that are specified in the room node graphs for the level")]
+    #endregion Tooltip
+
     public List<RoomTemplateSO> roomTemplateList;
+
+    #region Header ROOM NODE GRAPHS FOR LEVEL
+    [Space(10)]
+    [Header("Room node graphs for level")]
+    #endregion
+
+    #region Toolip
+    [Tooltip("Populate this list with the room node graphs which should be randomly from for the level")]
+    #endregion Tooltip
+
+    public List<RoomNodeGraphSO> roomNodeGraphList;
 }
