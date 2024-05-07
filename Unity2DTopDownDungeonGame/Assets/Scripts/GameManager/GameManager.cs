a8f8e8fe6fc388dbf7e32f1539938467839f0cce
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-06 22:41:11 -0500

    Added Game Manager script

---
+++ Unity2DTopDownDungeonGame/Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class GameManager : SingletonMonobehaviour<GameManager>
+{
+    #region Header DUNGEON LEVELS
+    [Space(10)]
+    [Header("DUNGEON LEVELS")]
+    #endregion Header DUNGEON LEVELS
+
+    #region Tooltip
+    [Tooltip("Populate with the dungeon level scriptable objects")]
+    #endregion Tooltip
+
+    [SerializeField] private List<DungeonLevelSO> dungeonLevelList;
+
+    #region Tooltip
+    [Tooltip("Populate with the starting dungeon level for testing, first level = 0")]
+    #endregion Tooltip
+
+    [SerializeField] private int currentDungeonLevelListIndex = 0;
+
+    [HideInInspector] public GameState gameState;
+
+    private void Start()
+    {
+        gameState = GameState.gameStarted;
+    }
+
+    private void Update()
+    {
+        HandleGameState();
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gameState = GameState.gameStarted;
+        }
+    }
+
+    /// <summary>
+    /// Handle game state
+    /// </summary>
+    private void HandleGameState()
+    {
+        // handle game state
+        switch(gameState)
+        {
+            case GameState.gameStarted:
+                // play first level
+                PlayDungeonLevel(currentDungeonLevelListIndex);
+
+                gameState = GameState.playingLevel;
+
+                break;
+        }
+    }
+
+    private void PlayDungeonLevel(int currentDungeonLevelListIndex)
+    {
+
+    }
+}
