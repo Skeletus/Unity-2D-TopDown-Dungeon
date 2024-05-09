54bb36b14ce32c9f535280bb321a705d64229a66
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-08 21:56:44 -0500

    added generate dungeon function part one

--- Unity2DTopDownDungeonGame/Assets/Scripts/Dungeon/DungeonBuilder.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Dungeon/DungeonBuilder.cs
@@ -30,4 +30,71 @@ public class DungeonBuilder : SingletonMonobehaviour<DungeonBuilder>
     {
         roomNodeTypeList = GameResources.Instance.roomNodeTypeList;
     }
+
+    /// <summary>
+    /// Generate random dungeon, returns true if dungeon build, false if failed
+    /// </summary>
+    /// <param name="currentDungeonLevel"></param>
+    /// <returns></returns>
+    public bool GenerateDungeon(DungeonLevelSO currentDungeonLevel)
+    {
+        roomTemplateList = currentDungeonLevel.roomTemplateList;
+
+        // load scriptable game objects room templates into the dictionary
+        LoadRoomTemplatesIntoDictionary();
+
+        dungeonBuildSuccessful = false;
+        int dungeonBuildAttempts = 0;
+
+        // while dungeon build isn't successful and dungeon build attemps has reached the max attempts
+        while(!dungeonBuildSuccessful && dungeonBuildAttempts < Settings.maxDungeonBuildAttempts)
+        {
+            dungeonBuildAttempts++;
+
+            // select a random room node graph from the list
+            RoomNodeGraphSO roomNodeGraph = SelectRandomRoomNodeGraph(currentDungeonLevel.roomNodeGraphList);
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Select a random room node graph from the list of room node graph
+    /// </summary>
+    /// <param name="roomNodeGraphList"></param>
+    /// <returns></returns>
+    private RoomNodeGraphSO SelectRandomRoomNodeGraph(List<RoomNodeGraphSO> roomNodeGraphList)
+    {
+        if (roomNodeGraphList.Count > 0)
+        {
+            return roomNodeGraphList[UnityEngine.Random.Range(0, roomNodeGraphList.Count)];
+        }
+        else
+        {
+            Debug.Log("No room node graphs in list");
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Load the room templates into the dictionary
+    /// </summary>
+    private void LoadRoomTemplatesIntoDictionary()
+    {
+        // clear room template dictionary
+        roomTemplateDictionary.Clear();
+
+        // load room template list into dictionary
+        foreach(RoomTemplateSO roomTemplate in roomTemplateList)
+        {
+            if (!roomTemplateDictionary.ContainsKey(roomTemplate.guid))
+            {
+                roomTemplateDictionary.Add(roomTemplate.guid, roomTemplate);
+            }
+            else
+            {
+                Debug.Log("Duplicate room template key in: " + roomTemplateList);
+            }
+        }
+    }
 }
