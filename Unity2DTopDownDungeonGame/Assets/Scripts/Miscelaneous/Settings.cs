6ce774fcc0fbad9ce9e03ed49ddb9dd51356933c
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-08 21:08:59 -0500

    Added settings parameters for dungeon build

--- Unity2DTopDownDungeonGame/Assets/Scripts/Miscelaneous/Settings.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Miscelaneous/Settings.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public static class Settings
 {
+    #region DUNGEON BUILD SETTINGS
+    public const int maxDungeonReBuildAttemptsFromRoomGraph = 1000;
+    public const int maxDungeonBuildAttempts = 10;
+    #endregion
+
     #region ROOM SETTINGS
     /* max number of child corridors leading from a room - max should be 3 although this is not recomended
      since it cause the dungeon building to fail since the rooms are more likely to not fit together*/
