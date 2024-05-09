ae1c67aab7dfedbedc7083b60f96cce07a959fa9
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-08 21:24:59 -0500

    created dungeon builder class and attritbutes

---
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Dungeon/DungeonBuilder.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class DungeonBuilder : SingletonMonobehaviour<DungeonBuilder>
+{
+    public Dictionary<string, Room> dungeonBuilderRoomDictionary = new Dictionary<string, Room>();
+    private Dictionary<string, RoomTemplateSO> roomTemplateDictionary = new Dictionary<string, RoomTemplateSO>();
+    private List<RoomTemplateSO> roomTemplateList = null;
+    private RoomNodeTypeListSO roomNodeTypeList;
+    private bool dungeonBuildSuccessful;
+}
