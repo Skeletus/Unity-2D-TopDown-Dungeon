46fb8b6d089308718e0928e4a207597414031d6d
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-11 17:26:36 -0500

    updated InstantiatedRoom script

--- Unity2DTopDownDungeonGame/Assets/Scripts/Dungeon/InstantiatedRoom.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Dungeon/InstantiatedRoom.cs
@@ -1,18 +1,91 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
+[DisallowMultipleComponent]
+[RequireComponent(typeof(BoxCollider2D))]
 public class InstantiatedRoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector] public Room room;
+    [HideInInspector] public Grid grid;
+    [HideInInspector] public Tilemap groundTilemap;
+    [HideInInspector] public Tilemap decoration1Tilemap;
+    [HideInInspector] public Tilemap decoration2Tilemap;
+    [HideInInspector] public Tilemap frontTilemap;
+    [HideInInspector] public Tilemap collisionTilemap;
+    [HideInInspector] public Tilemap minimapTilemap;
+    [HideInInspector] public Bounds roomColliderBounds;
+
+    private BoxCollider2D boxCollider2D;
+
+    private void Awake()
     {
-        
+        boxCollider2D = GetComponent<BoxCollider2D>();
+
+        // save room collider bounds
+        roomColliderBounds = boxCollider2D.bounds;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Initialise the instatiated room
+    /// </summary>
+    /// <param name="roomGameObject"></param>
+    public void Initialise(GameObject roomGameObject)
     {
-        
+        PopulateTileMapMemberVariables(roomGameObject);
+
+        DisableCollisionTilemapRenderer();
+    }
+
+    /// <summary>
+    /// Disable collision tilemap renderer
+    /// </summary>
+    private void DisableCollisionTilemapRenderer()
+    {
+        // disable collision tilemap render
+        collisionTilemap.gameObject.GetComponent<TilemapRenderer>().enabled = false;
+    }
+
+    /// <summary>
+    /// populate the tilemap and grid variables
+    /// </summary>
+    /// <param name="roomGameObject"></param>
+    private void PopulateTileMapMemberVariables(GameObject roomGameObject)
+    {
+        // get the grid Component
+        grid = roomGameObject.GetComponentInChildren<Grid>();
+
+        // get tilemaps in children
+        Tilemap[] tilemaps = roomGameObject.GetComponentsInChildren<Tilemap>();
+
+        foreach(Tilemap tilemap in tilemaps) 
+        {
+            if (tilemap.gameObject.tag == "groundTilemap")
+            {
+                groundTilemap = tilemap;
+            }
+            else if (tilemap.gameObject.tag == "decoration1Tilemap")
+            {
+                decoration1Tilemap = tilemap;
+            }
+            else if (tilemap.gameObject.tag == "decoration2Tilemap")
+            {
+                decoration2Tilemap = tilemap;
+            }
+            else if (tilemap.gameObject.tag == "frontTilemap")
+            {
+                frontTilemap = tilemap;
+            }
+            else if (tilemap.gameObject.tag == "collisionTilemap")
+            {
+                collisionTilemap = tilemap;
+            }
+            else if (tilemap.gameObject.tag == "minimapTilemap")
+            {
+                minimapTilemap = tilemap;
+            }
+        }
     }
 }
