b03b7194f4767b8da1f368dd101e5925ae5a1976
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-24 22:00:38 -0500

    added player details scriptable object

---
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Player/PlayerDetailsSO.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerDetails_", menuName = "Scriptable Objects/Player/Player Details")]
+public class PlayerDetailsSO : ScriptableObject
+{
+    #region Header PLAYER BASE DETAILS
+    [Space(10)]
+    [Header("PLAYER BASE DETAILS")]
+    #endregion
+    #region Tooltip
+    [Tooltip("Player character name")]
+    #endregion
+    public string playerCharacterName;
+
+    #region Tooltip
+    [Tooltip("Prefab gameobject for the player")]
+    #endregion
+    public GameObject playerPrefab;
+
+    #region Tooltip
+    [Tooltip("Player runtime animator controller")]
+    #endregion
+    public RuntimeAnimatorController runtimeAnimatorController;
+
+    #region Header PLAYER HEALTH
+    [Space(10)]
+    [Header("HEALTH")]
+    #endregion
+    #region Tooltip
+    [Tooltip("Player starting health amount")]
+    #endregion
+    public int playerHealthAmount;
+
+    #region Header OTHER
+    [Space(10)]
+    [Header("OTHER")]
+    #endregion
+    #region Tooltip
+    [Tooltip("Player icon sprite to be used in the minimap")]
+    #endregion
+    public Sprite playerMiniMapIcon;
+
+    #region Tooltip
+    [Tooltip("Player hand sprite")]
+    #endregion
+    public Sprite playerHandSprite;
+
+    #region Validation
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEmptyStrings(this, nameof(playerCharacterName), playerCharacterName);
+        HelperUtilities.ValidateCheckNullValues(this, nameof(playerPrefab), playerPrefab);
+        HelperUtilities.ValidateCheckPositiveValues(this, nameof(playerHealthAmount), playerHealthAmount, false);
+        HelperUtilities.ValidateCheckNullValues(this, nameof(playerMiniMapIcon), playerMiniMapIcon);
+        HelperUtilities.ValidateCheckNullValues(this, nameof(playerHandSprite), playerHandSprite);
+        HelperUtilities.ValidateCheckNullValues(this, nameof(runtimeAnimatorController), runtimeAnimatorController);
+    }
+#endif
+    #endregion
+}
