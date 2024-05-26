62b63538d5044d5c1352d58d1225c7b6f752c3da
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-05-25 20:57:28 -0500

    added player class and setting up require components

---
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+#region REQUIRE COMPONENTS
+[RequireComponent(typeof(SortingGroup))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+[DisallowMultipleComponent]
+#endregion REQUIRE COMPONENTS
+public class Player : MonoBehaviour
+{
+
+}
