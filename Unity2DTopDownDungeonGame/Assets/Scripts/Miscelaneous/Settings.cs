abfcb192ef546c7fe88fe30574227dac9688e5e4
Skeletus <Samol_88@hotmail.com>
2024-07-16 20:23:30 -0500

    hashing animator parameters

--- Unity2DTopDownDungeonGame/Assets/Scripts/Miscelaneous/Settings.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Miscelaneous/Settings.cs
@@ -14,4 +14,16 @@ public static class Settings
      since it cause the dungeon building to fail since the rooms are more likely to not fit together*/
     public const int maxChildCorridors = 3;
     #endregion
+
+    #region ANIMATOR SETTINGS
+    // animator parameteres - player
+    public static int aimUp = Animator.StringToHash("aimUp");
+    public static int aimDown = Animator.StringToHash("aimDown");
+    public static int aimUpRight = Animator.StringToHash("aimUpRight");
+    public static int aimUpLeft = Animator.StringToHash("aimUpLeft");
+    public static int aimRight = Animator.StringToHash("aimRight");
+    public static int aimLeft = Animator.StringToHash("aimLeft");
+    public static int isIdle = Animator.StringToHash("isIdle");
+    public static int isMoving = Animator.StringToHash("isMoving");
+    #endregion
 }
