df20ea0236571672a0e59be1dbef4725de0d778c
Skeletus <Samol_88@hotmail.com>
2024-07-17 16:36:10 -0500

    Created AnimatePlayer publisher for set player animations

---
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Player/AnimatePlayer.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+[DisallowMultipleComponent]
+public class AnimatePlayer : MonoBehaviour
+{
+    private Player player;
+
+    private void Awake()
+    {
+        // load components
+        player = GetComponent<Player>();
+    }
+
+    private void OnEnable()
+    {
+        // subscribe to idle event
+        player.idleEvent.OnIdle += IdleEvent_OnIdle;
+
+        // subscribe to weapon aim event
+        player.aimWeaponEvent.OnWeaponAim += AimWeaponEvent_OnWeaponAim;
+    }
+
+    private void OnDisable()
+    {
+        // unsubscribe to idle event
+        player.idleEvent.OnIdle -= IdleEvent_OnIdle;
+
+        // unsubscribe to weapon aim event
+        player.aimWeaponEvent.OnWeaponAim -= AimWeaponEvent_OnWeaponAim;
+    }
+
+    /// <summary>
+    /// On weapon aim event handler
+    /// </summary>
+    /// <param name="arg1"></param>
+    /// <param name="arg2"></param>
+    private void AimWeaponEvent_OnWeaponAim(AimWeaponEvent aimWeaponEvent, AimWeaponEventArgs aimWeaponEventArgs)
+    {
+        IntializeAimWeaponParameters();
+
+        SetAimWeaponAnimationParameters(aimWeaponEventArgs.aimDirection);
+    }
+
+    /// <summary>
+    /// Set aim animation parameters
+    /// </summary>
+    /// <param name="aimDirection"></param>
+    private void SetAimWeaponAnimationParameters(AimDirection aimDirection)
+    {
+        // set aim direction
+        switch(aimDirection)
+        {
+            case AimDirection.Up:
+                player.animator.SetBool(Settings.aimUp, true);
+                break;
+            case AimDirection.UpRight:
+                player.animator.SetBool(Settings.aimUpRight, true);
+                break;
+            case AimDirection.UpLeft:
+                player.animator.SetBool(Settings.aimUpLeft, true);
+                break;
+            case AimDirection.Right:
+                player.animator.SetBool(Settings.aimRight, true);
+                break;
+            case AimDirection.Left:
+                player.animator.SetBool(Settings.aimLeft, true);
+                break;
+            case AimDirection.Down:
+                player.animator.SetBool(Settings.aimDown, true);
+                break;
+
+        }
+    }
+
+    /// <summary>
+    /// Initialize aim animation parameters
+    /// </summary>
+    private void IntializeAimWeaponParameters()
+    {
+        player.animator.SetBool(Settings.aimUp, false);
+        player.animator.SetBool(Settings.aimUpRight, false);
+        player.animator.SetBool(Settings.aimUpLeft, false);
+        player.animator.SetBool(Settings.aimRight, false);
+        player.animator.SetBool(Settings.aimLeft, false);
+        player.animator.SetBool(Settings.aimDown, false);
+    }
+
+    /// <summary>
+    /// on idle event handler
+    /// </summary>
+    /// <param name="obj"></param>
+    private void IdleEvent_OnIdle(IdleEvent obj)
+    {
+        SetIdleAnimationParameters();
+    }
+
+    /// <summary>
+    /// Set movement animation parameters
+    /// </summary>
+    private void SetIdleAnimationParameters()
+    {
+        player.animator.SetBool(Settings.isMoving, false);
+        player.animator.SetBool(Settings.isIdle, true);
+    }
+}
