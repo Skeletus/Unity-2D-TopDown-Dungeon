dd943fbbeda5fac08fef9a44ed461235b640ca39
Skeletus <Samol_88@hotmail.com>
2024-07-17 18:17:08 -0500

    Created Movement By Velocity Subscriber

---
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Movement/MovementByVelocity.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(MovementByVelocityEvent))]
+[DisallowMultipleComponent]
+public class MovementByVelocity : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    private MovementByVelocityEvent movementByVelocityEvent;
+
+    private void Awake()
+    {
+        // load components
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        movementByVelocityEvent = GetComponent<MovementByVelocityEvent>();
+    }
+
+    private void OnEnable()
+    {
+        // subscribte to movement event
+        movementByVelocityEvent.OnMovementByVelocity += MovementByVelocityEvent_OnMovementByVelocity;
+    }
+
+    private void OnDisable()
+    {
+        // unsubscribe to movement event
+        movementByVelocityEvent.OnMovementByVelocity -= MovementByVelocityEvent_OnMovementByVelocity;
+    }
+
+    // on movement event
+    private void MovementByVelocityEvent_OnMovementByVelocity(MovementByVelocityEvent movementByVelocityEvent,
+        MovementByVelocityArgs movementByVelocityArgs)
+    {
+        MoveRigidBody(movementByVelocityArgs.moveDirection, movementByVelocityArgs.moveSpeed);
+    }
+
+    /// <summary>
+    /// Move the rigigbody component
+    /// </summary>
+    /// <param name="moveDirection"></param>
+    /// <param name="moveSpeed"></param>
+    private void MoveRigidBody(Vector2 moveDirection, float moveSpeed)
+    {
+        // ensure to rb collision is set to continous
+        rigidBody2D.velocity += moveDirection * moveDirection;
+    }
+}
