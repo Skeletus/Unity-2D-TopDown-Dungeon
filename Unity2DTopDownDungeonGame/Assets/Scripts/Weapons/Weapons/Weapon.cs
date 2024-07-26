9bdd67b5cd44074dd1f78ce732f2a56a5473efdc
Skeletus <Samol_88@hotmail.com>
2024-07-26 16:26:52 -0500

    Created Weapon Classs

---
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Weapons/Weapons/Weapon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon
+{
+    public WeaponDetailsSO weaponDetails;
+    public int weaponListPosition;
+    public float weaponReloadTimer;
+    public int weaponClipRemainingAmmo;
+    public int weaponRemainingAmmo;
+    public bool isWeaponReloading;
+}
