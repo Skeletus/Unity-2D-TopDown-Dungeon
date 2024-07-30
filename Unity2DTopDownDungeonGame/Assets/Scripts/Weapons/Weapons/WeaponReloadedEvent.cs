2cdc0132cddff8bd2b6ca05a2c6bc3053f792748
Skeletus <Samol_88@hotmail.com>
2024-07-29 23:35:04 -0500

    Created weapon reloaded event

---
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Weapons/Weapons/WeaponReloadedEvent.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class WeaponReloadedEvent : MonoBehaviour
+{
+    public event Action<WeaponReloadedEvent, WeaponReloadedEventArgs> OnWeaponReloaded;
+
+    public void CallWeaponReloadedEvent(Weapon weapon)
+    {
+        OnWeaponReloaded?.Invoke(this, new WeaponReloadedEventArgs()
+        {
+            weapon = weapon
+        });
+    }
+}
+
+public class WeaponReloadedEventArgs : EventArgs
+{
+    public Weapon weapon;
+}
