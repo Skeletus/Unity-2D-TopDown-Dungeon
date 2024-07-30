839fdfb961c1fc4d58c5221319a1cc65dfb48454
Skeletus <Samol_88@hotmail.com>
2024-07-30 14:00:53 -0500

    Updated fire weapon event added new bool parameter

--- Unity2DTopDownDungeonGame/Assets/Scripts/Player/PlayerControl.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Player/PlayerControl.cs
@@ -12,6 +12,7 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private MovementDetailsSO movementDetails;
 
     private Player player;
+    private bool leftMouseDownPreviousFrame = false;
     private int currentWeaponIndex = 1;
     private float moveSpeed;
     private Coroutine playerRollCoroutine;
@@ -105,8 +106,14 @@ public class PlayerControl : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             // trigger fire weapon event
-            player.fireWeaponEvent.CallFireWeaponEvent(true, playerAimDirection, playerAngleDegrees,
+            player.fireWeaponEvent.CallFireWeaponEvent(true, leftMouseDownPreviousFrame, playerAimDirection, playerAngleDegrees,
                 weaponAngleDegrees, weaponDirection);
+
+            leftMouseDownPreviousFrame = true;
+        }
+        else
+        {
+            leftMouseDownPreviousFrame = false;
         }
     }
 
