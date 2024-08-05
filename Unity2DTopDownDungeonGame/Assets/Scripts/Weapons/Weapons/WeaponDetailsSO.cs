dca84dea5a7fae3a0950ceab4690997051b08fbe
Skeletus <Samol_88@hotmail.com>
2024-08-05 16:34:34 -0500

    Added weapon shoot effect prefabs to pool manager

--- Unity2DTopDownDungeonGame/Assets/Scripts/Weapons/Weapons/WeaponDetailsSO.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Weapons/Weapons/WeaponDetailsSO.cs
@@ -35,12 +35,10 @@ public class WeaponDetailsSO : ScriptableObject
     #endregion Tooltip
     public AmmoDetailsSO weaponCurrentAmmo;
 
-    /*
     #region Tooltip
     [Tooltip("Weapon shoot effect SO - contains particle effecct parameters to be used in conjunction with the weaponShootEffectPrefab ")]
     #endregion Tooltip
     public WeaponShootEffectSO weaponShootEffect;
-    */
 
     #region Tooltip
     [Tooltip("The firing sound effect SO for the weapon")]
