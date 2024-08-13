d570d5045d9b6c8a1d7681a210ba549743f1f89f
Skeletus <Samol_88@hotmail.com>
2024-08-13 18:34:09 -0500

    Updated Enemy class to optimze path fiding

--- Unity2DTopDownDungeonGame/Assets/Scripts/Enemies/Enemy.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Enemies/Enemy.cs
@@ -50,7 +50,18 @@ public class Enemy : MonoBehaviour
     {
         this.enemyDetails = enemyDetails;
 
+        SetEnemyMovementUpdateFrame(enemySpawnNumber);
+
         SetEnemyAnimationSpeed();
+    }
+
+    /// <summary>
+    /// Set enemy movement update frame
+    /// </summary>
+    private void SetEnemyMovementUpdateFrame(int enemySpawnNumber)
+    {
+        // Set frame number that enemy should process it's updates
+        enemyMovementAI.SetUpdateFrameNumber(enemySpawnNumber % Settings.targetFrameRateToSpreadPathfindingOver);
     }
 
     /// <summary>
