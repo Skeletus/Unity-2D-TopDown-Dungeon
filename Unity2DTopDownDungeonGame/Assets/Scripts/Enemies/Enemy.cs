87df6dcf843de6171e152f6d87a0734fd474ae5e
Skeletus <Samol_88@hotmail.com>
2024-08-14 21:20:53 -0500

    Added required component for weapons in enemy class

--- Unity2DTopDownDungeonGame/Assets/Scripts/Enemies/Enemy.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Enemies/Enemy.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 using UnityEngine.Rendering;
 
 #region REQUIRE COMPONENTS
+[RequireComponent(typeof(EnemyWeaponAI))]
+[RequireComponent(typeof(AimWeaponEvent))]
+[RequireComponent(typeof(AimWeapon))]
+[RequireComponent(typeof(FireWeaponEvent))]
+[RequireComponent(typeof(FireWeapon))]
+[RequireComponent(typeof(SetActiveWeaponEvent))]
+[RequireComponent(typeof(ActiveWeapon))]
+[RequireComponent(typeof(WeaponFiredEvent))]
+[RequireComponent(typeof(ReloadWeaponEvent))]
+[RequireComponent(typeof(ReloadWeapon))]
+[RequireComponent(typeof(WeaponReloadedEvent))]
 [RequireComponent(typeof(EnemyMovementAI))]
 [RequireComponent(typeof(MovementToPositionEvent))]
 [RequireComponent(typeof(MovementToPosition))]
@@ -31,6 +42,8 @@ public class Enemy : MonoBehaviour
     [HideInInspector] public Animator animator;
 
     private EnemyMovementAI enemyMovementAI;
+    private FireWeapon fireWeapon;
+    private SetActiveWeaponEvent setActiveWeaponEvent;
     private CircleCollider2D circleCollider2D;
     private PolygonCollider2D polygonCollider2D;
     private MaterializeEffect materializeEffect;
@@ -38,6 +51,10 @@ public class Enemy : MonoBehaviour
     private void Awake()
     {
         // Load components
+        aimWeaponEvent = GetComponent<AimWeaponEvent>();
+        fireWeaponEvent = GetComponent<FireWeaponEvent>();
+        fireWeapon = GetComponent<FireWeapon>();
+        setActiveWeaponEvent = GetComponent<SetActiveWeaponEvent>();
         enemyMovementAI = GetComponent<EnemyMovementAI>();
         movementToPositionEvent = GetComponent<MovementToPositionEvent>();
         idleEvent = GetComponent<IdleEvent>();
@@ -57,6 +74,8 @@ public class Enemy : MonoBehaviour
 
         SetEnemyMovementUpdateFrame(enemySpawnNumber);
 
+        SetEnemyStartingWeapon();
+
         SetEnemyAnimationSpeed();
 
         // Materialise enemy
@@ -70,6 +89,22 @@ public class Enemy : MonoBehaviour
     {
         // Set frame number that enemy should process it's updates
         enemyMovementAI.SetUpdateFrameNumber(enemySpawnNumber % Settings.targetFrameRateToSpreadPathfindingOver);
+    }
+
+    /// <summary>
+    /// Set enemy starting weapon as per the weapon details SO
+    /// </summary>
+    private void SetEnemyStartingWeapon()
+    {
+        // Process if enemy has a weapon
+        if (enemyDetails.enemyWeapon != null)
+        {
+            Weapon weapon = new Weapon() { weaponDetails = enemyDetails.enemyWeapon, weaponReloadTimer = 0f, weaponClipRemainingAmmo = enemyDetails.enemyWeapon.weaponClipAmmoCapacity, weaponRemainingAmmo = enemyDetails.enemyWeapon.weaponAmmoCapacity, isWeaponReloading = false };
+
+            //Set weapon for enemy
+            setActiveWeaponEvent.CallSetActiveWeaponEvent(weapon);
+
+        }
     }
 
     /// <summary>
@@ -101,5 +136,8 @@ public class Enemy : MonoBehaviour
 
         // Enable/Disable movement AI
         enemyMovementAI.enabled = isEnabled;
+
+        // Enable / Disable Fire Weapon
+        fireWeapon.enabled = isEnabled;
     }
 }
