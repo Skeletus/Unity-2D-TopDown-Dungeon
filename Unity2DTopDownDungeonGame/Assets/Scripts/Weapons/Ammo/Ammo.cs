2d8fe268f56b82698293aeacbd2fdf600458cbe3
Skeletus <Samol_88@hotmail.com>
2024-08-15 23:47:21 -0500

    Create DealDamage func in Ammo component

--- Unity2DTopDownDungeonGame/Assets/Scripts/Weapons/Ammo/Ammo.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Weapons/Ammo/Ammo.cs
@@ -56,10 +56,23 @@ public class Ammo : MonoBehaviour, IFireable
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Deal Damage To Collision Object
+        DealDamage(collision);
+
         // Show ammo hit effect
         AmmoHitEffect();
 
         DisableAmmo();
+    }
+
+    private void DealDamage(Collider2D collision)
+    {
+        Health health = collision.GetComponent<Health>();
+
+        if (health != null)
+        {
+            health.TakeDamage(ammoDetails.ammoDamage);
+        }
     }
 
     private void SetAmmoMaterial(Material ammoMaterial)
