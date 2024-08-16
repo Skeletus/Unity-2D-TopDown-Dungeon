50ae2f4f6d0b92748d19de3932d470299d55ee73
Skeletus <Samol_88@hotmail.com>
2024-08-15 22:51:55 -0500

    Updated Health component added health event  publisher

--- Unity2DTopDownDungeonGame/Assets/Scripts/Health/Health.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Health/Health.cs
@@ -2,10 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(HealthEvent))]
+[DisallowMultipleComponent]
 public class Health : MonoBehaviour
 {
+    [HideInInspector] public bool isDamageable = true;
+
     private int startingHealth;
     private int currentHealth;
+    private HealthEvent healthEvent;
+
+    private void Awake()
+    {
+        //Load compnents
+        healthEvent = GetComponent<HealthEvent>();
+    }
+
+    private void Start()
+    {
+        // Trigger a health event for UI update
+        CallHealthEvent(0);
+
+    }
+
+    private void CallHealthEvent(int damageAmount)
+    {
+        // Trigger health event
+        healthEvent.CallHealthChangedEvent(((float)currentHealth / (float)startingHealth), currentHealth, damageAmount);
+    }
+
+    /// <summary>
+    /// Public method called when damage is taken
+    /// </summary>
+    public void TakeDamage(int damageAmount)
+    {
+
+        if (isDamageable)
+        {
+            currentHealth -= damageAmount;
+            CallHealthEvent(damageAmount);
+        }
+    }
 
     /// <summary>
     /// Set starting health
