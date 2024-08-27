53c7ea5660c680403c31ac6c28bbce9aab218a33
Skeletus <Samol_88@hotmail.com>
2024-08-26 21:05:03 -0500

    Updated PlayerControl - added UseItemInput func

--- Unity2DTopDownDungeonGame/Assets/Scripts/Player/PlayerControl.cs
+++ Unity2DTopDownDungeonGame/Assets/Scripts/Player/PlayerControl.cs
@@ -58,6 +58,9 @@ public class PlayerControl : MonoBehaviour
         // process player weapon input
         WeaponInput();
 
+        // Process player use item input
+        UseItemInput();
+
         // player roll cooldown timer
         PlayerRollCooldownTimer();
     }
@@ -365,6 +368,31 @@ public class PlayerControl : MonoBehaviour
         if (playerRollCooldownTimer >= 0f)
         {
             playerRollCooldownTimer -= Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// Use the nearest item within 2 unity units from the player
+    /// </summary>
+    private void UseItemInput()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            float useItemRadius = 2f;
+
+            // Get any 'Useable' item near the player
+            Collider2D[] collider2DArray = Physics2D.OverlapCircleAll(player.GetPlayerPosition(), useItemRadius);
+
+            // Loop through detected items to see if any are 'useable'
+            foreach (Collider2D collider2D in collider2DArray)
+            {
+                IUseable iUseable = collider2D.GetComponent<IUseable>();
+
+                if (iUseable != null)
+                {
+                    iUseable.UseItem();
+                }
+            }
         }
     }
 
